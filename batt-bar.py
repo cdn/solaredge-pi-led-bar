84a3da67cee57f69f2a86a1659f8c27f79d6c35e
cdn <cdn+github@chrs.nl>
2018-01-31 14:14:07 +0000

    Update batt-bar.py
    Pseudo pep8 pass

--- batt-bar.py
+++ batt-bar.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
 import RPi.GPIO as GPIO
-import requests, simplejson as json, sys, time
+import requests
+import simplejson as json
+import sys
+import time
 
 #sudo pip3 install tenacity
 
-#from tenacity import retry, stop_after_attempt, wait_combine, wait_fixed, wait_jitter
+#from tenacity import retry, stop_after_attempt
+#from tenacity import wait_combine, wait_fixed, wait_jitter
 
 # Suppress output pertaining to use of SPI pins as general GPIO (tautology)
 GPIO.setwarnings(False)
@@ -23,7 +27,7 @@ BAR_Y2 = 5
 BAR_G1 = 7
 BAR_G2 = 8
 BAR_G3 = 11
-BAR_G4 = 9 # no apparent output
+BAR_G4 = 9  # no apparent output
 BAR_G4 = 25
 BAR_G5 = 10
 
@@ -36,81 +40,83 @@ SE_SITE = 'nnnnnn'
 SE_AKEY = 'ALPHANUMERIC0123456789ABCEDFGHIJ'
 
 # array/tuple pin layout
-bar = [26,19,13,6,5,7,8,11,25,10]
+bar = [26, 19, 13, 6, 5, 7, 8, 11, 25, 10]
 GPIO.setup(bar, GPIO.OUT)
 
-#@retry(wait=wait_combine(wait_fixed(10), wait_jitter(30)), stop=stop_after_attempt(5))
+#@retry(wait=wait_combine(wait_fixed(10), wait_jitter(30)),
+# stop=stop_after_attempt(5))
 r = requests.session()
 
 # URL to SolarEdge API
-u = 'https://monitoringapi.solaredge.com/site/' + SE_SITE + '/currentPowerFlow?api_key=' + SE_AKEY
+u = 'https://monitoringapi.solaredge.com/site/' + SE_SITE
+u += '/currentPowerFlow?api_key=' + SE_AKEY
 
 while 1:
-  try:
-    j = r.get(u, timeout=10)
-    if 'STORAGE' not in j.text:
-      print('no data')
-      time.sleep(900)
-      j = r.get(u, timeout=10)
+    try:
+        j = r.get(u, timeout=10)
+        if 'STORAGE' not in j.text:
+            print('no data')
+            time.sleep(900)
+            j = r.get(u, timeout=10)
 #      sys.exit()
 
 # STORAGE key in j.json()['siteCurrentPowerFlow']
 #    if 'STORAGE' in j.text:
-    t = j.json()['siteCurrentPowerFlow']['STORAGE']
+        t = j.json()['siteCurrentPowerFlow']['STORAGE']
 
-    c = int(t['chargeLevel'])
+        c = int(t['chargeLevel'])
 
 # remainder from division
-    d = c % 10
+        d = c % 10
 
 # index to pinout array for LED PWM
-    e = int(c / 10)
+        e = int(c / 10)
 
 #    print(e)
 
-    if e<10:
-      print(bar[e])
+        if e<10:
+            print(bar[e])
 
 # output selection control structure, tuple addressing
 
-    if c == 100:
-        GPIO.output(bar, (1,1,1,1,1,1,1,1,1,1))
-    elif c >= 90:
-        GPIO.output(bar, (1,1,1,1,1,1,1,1,1,0))
-    elif c >= 80:
-        GPIO.output(bar, (1,1,1,1,1,1,1,1,0,0))
-    elif c >= 70:
-        GPIO.output(bar, (1,1,1,1,1,1,1,0,0,0))
-    elif c >= 60:
-        GPIO.output(bar, (1,1,1,1,1,1,0,0,0,0))
-    elif c >= 50:
-        GPIO.output(bar, (1,1,1,1,1,0,0,0,0,0))
-    elif c >= 40:
-        GPIO.output(bar, (1,1,1,1,0,0,0,0,0,0))
-    elif c >= 30:
-        GPIO.output(bar, (1,1,1,0,0,0,0,0,0,0))
-    elif c >= 20:
-        GPIO.output(bar, (1,1,0,0,0,0,0,0,0,0))
-    elif c >= 10:
-        GPIO.output(bar, (1,0,0,0,0,0,0,0,0,0))
-    else:
-        GPIO.output(bar, 0)
+        if c == 100:
+            GPIO.output(bar, (1, 1, 1, 1, 1, 1, 1, 1, 1, 1))
+        elif c >= 90:
+            GPIO.output(bar, (1, 1, 1, 1, 1, 1, 1, 1, 1, 0))
+        elif c >= 80:
+            GPIO.output(bar, (1, 1, 1, 1, 1, 1, 1, 1, 0, 0))
+        elif c >= 70:
+            GPIO.output(bar, (1, 1, 1, 1, 1, 1, 1, 0, 0, 0))
+        elif c >= 60:
+            GPIO.output(bar, (1, 1, 1, 1, 1, 1, 0, 0, 0, 0))
+        elif c >= 50:
+            GPIO.output(bar, (1, 1, 1, 1, 1, 0, 0, 0, 0, 0))
+        elif c >= 40:
+            GPIO.output(bar, (1, 1, 1, 1, 0, 0, 0, 0, 0, 0))
+        elif c >= 30:
+            GPIO.output(bar, (1, 1, 1, 0, 0, 0, 0, 0, 0, 0))
+        elif c >= 20:
+            GPIO.output(bar, (1, 1, 0, 0, 0, 0, 0, 0, 0, 0))
+        elif c >= 10:
+            GPIO.output(bar, (1, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+        else:
+            GPIO.output(bar, 0)
 
 # apply PWM with d*10 duty cycle on next level above (c - d)%
 
-    if d > 0:
-        p = GPIO.PWM(bar[e], 100)
-        p.start(int(d*10))
+        if d > 0:
+            p = GPIO.PWM(bar[e], 100)
+            p.start(int(d*10))
 
-    wait = 900
-    while wait>0:
-        print(wait)
-        time.sleep(1)
-        wait -= 1
-  except ConnectionError:
-    print('-error-')
-  except ConnectionResetError:
-    print('Reset')
-  except KeyboardInterrupt:
-   GPIO.cleanup()
-   sys.exit()
+        wait = 900
+        while wait > 0:
+            print(wait)
+            time.sleep(1)
+            wait -= 1
+    except ConnectionError:
+        print('-error-')
+    except ConnectionResetError:
+        print('Reset')
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        sys.exit()
