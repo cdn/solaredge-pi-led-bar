2e5c059b2fe8230df3c5446e19474a5f03330551
cdn <cdn+github@chrs.nl>
2018-07-04 17:05:41 +0100

    Clean up.

--- zero-bar.py
+++ zero-bar.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-#import RPi.GPIO as GPIO
 from gpiozero import LEDBarGraph
 import requests
 import simplejson as json
@@ -11,11 +10,6 @@ import time
 
 #from tenacity import retry, stop_after_attempt
 #from tenacity import wait_combine, wait_fixed, wait_jitter
-
-# Suppress output pertaining to use of SPI pins as general GPIO (tautology)
-#GPIO.setwarnings(False)
-# Broadcom GPIO pin numbering scheme
-#GPIO.setmode(GPIO.BCM)
 
 # Cross-strip outputs; via omission of
 #  I2C, GND pins (on subsequent physical implementation)
@@ -28,7 +22,6 @@ BAR_Y2 = 5
 BAR_G1 = 7
 BAR_G2 = 8
 BAR_G3 = 11
-BAR_G4 = 9  # no apparent output
 BAR_G4 = 25
 BAR_G5 = 10
 
@@ -38,15 +31,10 @@ BAR_G5 = 10
 # SolarEdge inverter identifier/API key
 
 SE_SITE = 'nnnnnn'
-SE_AKEY = 'VE94E5S3PJKL0CXIEA0ZYRI45PF03PRY'
+SE_AKEY = 'ALPHANUMERIC0123456789ABCEDFGHIJ'
 
 # array/tuple pin layout
-bar = [26, 19, 13, 6, 5, 7, 8, 11, 25, 10]
-bar = {26, 19, 13, 6, 5, 7, 8, 11, 25, 10}
-bar = (26, 19, 13, 6, 5, 7, 8, 11, 25, 10)
-#GPIO.setup(bar, GPIO.OUT)
-
-graph = LEDBarGraph(26, 19, 13, 6, 5, 7, 8, 11, 25, 10)
+graph = LEDBarGraph(26, 19, 13, 6, 5, 7, 8, 11, 25, 10, pwm=True)
 
 #@retry(wait=wait_combine(wait_fixed(10), wait_jitter(30)),
 # stop=stop_after_attempt(5))
@@ -71,49 +59,9 @@ while 1:
 
         c = int(t['chargeLevel'])
 
-# remainder from division
-        d = c % 10
-
-# index to pinout array for LED PWM
-        e = int(c / 10)
-
-#    print(e)
-
-        if e < 10:
-            print(bar[e])
-
 # output selection control structure, tuple addressing
 
         graph.value = c/100
-
- #       if c == 100:
- #           GPIO.output(bar, (1, 1, 1, 1, 1, 1, 1, 1, 1, 1))
- #       elif c >= 90:
- #           GPIO.output(bar, (1, 1, 1, 1, 1, 1, 1, 1, 1, 0))
- #       elif c >= 80:
- #           GPIO.output(bar, (1, 1, 1, 1, 1, 1, 1, 1, 0, 0))
- #       elif c >= 70:
- #           GPIO.output(bar, (1, 1, 1, 1, 1, 1, 1, 0, 0, 0))
- #       elif c >= 60:
- #           GPIO.output(bar, (1, 1, 1, 1, 1, 1, 0, 0, 0, 0))
- #       elif c >= 50:
- #           GPIO.output(bar, (1, 1, 1, 1, 1, 0, 0, 0, 0, 0))
- #       elif c >= 40:
- #           GPIO.output(bar, (1, 1, 1, 1, 0, 0, 0, 0, 0, 0))
- #       elif c >= 30:
- #           GPIO.output(bar, (1, 1, 1, 0, 0, 0, 0, 0, 0, 0))
- #       elif c >= 20:
- #           GPIO.output(bar, (1, 1, 0, 0, 0, 0, 0, 0, 0, 0))
- #       elif c >= 10:
- #           GPIO.output(bar, (1, 0, 0, 0, 0, 0, 0, 0, 0, 0))
- #       else:
- #           GPIO.output(bar, 0)
-
-# apply PWM with d*10 duty cycle on next level above (c - d)%
-
- #       if d > 0:
- #           p = GPIO.PWM(bar[e], 100)
- #           p.start(int(d*10))
 
         wait = 900
         while wait > 0:
@@ -125,7 +73,6 @@ while 1:
     except ConnectionResetError:
         print('Reset')
     except KeyboardInterrupt:
-#        GPIO.cleanup()
         graph.close()
 #        gpiozero.Factory.close()
         sys.exit()
